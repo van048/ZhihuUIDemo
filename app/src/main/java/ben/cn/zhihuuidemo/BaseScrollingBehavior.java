430e3ff4961e6ffd359943916e0c344c231f3c73
van048 <van048@126.com>
2017-04-15 20:48:10 +0800

    make abstract for extension

---
+++ app/src/main/java/ben/cn/zhihuuidemo/BaseScrollingBehavior.java
@@ -0,0 +1,76 @@
+package ben.cn.zhihuuidemo;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+abstract class BaseScrollingBehavior extends CoordinatorLayout.Behavior {
+    private static final int ANIM_OUT_THRESHOLD = 50;
+    private static final int ANIM_IN_THRESHOLD = -ANIM_OUT_THRESHOLD / 2;
+    private int mCirculatingDy;
+    private boolean mCirculatingDown;
+    private boolean mLastScrollDirectionUp; // record whether the last scroll direction is up
+
+    BaseScrollingBehavior(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        mCirculatingDy = 0;
+        mCirculatingDown = true;
+        // the first anim must be out, so last must be in
+        mLastScrollDirectionUp = true;
+
+        setupAnimator();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        // must keep moving in the same direction to circulate
+        // update the circulating dy
+        if (dyConsumed > 0) {
+            // scrolling down
+            if (mCirculatingDown) {
+                mCirculatingDy += dyConsumed;
+            } else {
+                // up changed to down, restart circulating
+                mCirculatingDy = dyConsumed;
+            }
+            mCirculatingDown = true;
+        } else if (dyConsumed < 0) {
+            if (mCirculatingDown) {
+                mCirculatingDy = dyConsumed;
+            } else {
+                mCirculatingDy += dyConsumed;
+            }
+            mCirculatingDown = false;
+        }
+
+        if (mCirculatingDy >= ANIM_OUT_THRESHOLD) {
+            // avoid repeat
+            if (mLastScrollDirectionUp) {
+                mLastScrollDirectionUp = false;
+                startAnimDown();
+                mCirculatingDy = 0; // reset
+            }
+        } else if (mCirculatingDy < ANIM_IN_THRESHOLD) {
+            // avoid repeat
+            if (!mLastScrollDirectionUp) {
+                mLastScrollDirectionUp = true;
+                startAnimUp();
+                mCirculatingDy = 0;
+            }
+        }
+    }
+
+    abstract void startAnimUp();
+
+    abstract void startAnimDown();
+
+    abstract void setupAnimator();
+}
--- app/src/main/java/ben/cn/zhihuuidemo/ScrollingHeaderBehavior.java
+++ app/src/main/java/ben/cn/zhihuuidemo/ScrollingHeaderBehavior.java
@@ -3,73 +3,30 @@ package ben.cn.zhihuuidemo;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class ScrollingHeaderBehavior extends CoordinatorLayout.Behavior {
-    private static final long ANIM_DURATION = 200;
-    private static final int ANIM_OUT_THRESHOLD = 50;
-    private static final int ANIM_IN_THRESHOLD = -ANIM_OUT_THRESHOLD / 2;
-    private final int mHeaderViewHeight;
-    private int mCirculatingDy;
-    private boolean mCirculatingDown;
-    private ValueAnimator mHeaderViewAnimator;
-    private boolean mLastScrollDirectionUp; // record whether the last scroll direction is up
-    private int mNewTop; // record the changing top during animation
+public class ScrollingHeaderBehavior extends BaseScrollingBehavior {
     private View mHeaderView;
+    private final int mHeaderViewHeight;
+    private static final long ANIM_DURATION = 200;
+    private ValueAnimator mHeaderViewAnimator;
+    private int mNewTop; // record the changing top during animation
 
     public ScrollingHeaderBehavior(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-
-        mCirculatingDy = 0;
-        mCirculatingDown = true;
-        mHeaderViewHeight = (int) context.getResources().getDimension(R.dimen.main_header_height);
-
         setupAnimator();
-    }
-
-    @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
-        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+        mHeaderViewHeight = (int) context.getResources().getDimension(R.dimen.main_header_height);
     }
 
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
         mHeaderView = child;
-        // must keep moving in the same direction to circulate
-        // update the circulating dy
-        if (dyConsumed > 0) {
-            // scrolling down
-            if (mCirculatingDown) {
-                mCirculatingDy += dyConsumed;
-            } else {
-                // up changed to down, restart circulating
-                mCirculatingDy = dyConsumed;
-            }
-            mCirculatingDown = true;
-        } else if (dyConsumed < 0) {
-            if (mCirculatingDown) {
-                mCirculatingDy = dyConsumed;
-            } else {
-                mCirculatingDy += dyConsumed;
-            }
-            mCirculatingDown = false;
-        }
-
-        if (mCirculatingDy >= ANIM_OUT_THRESHOLD) {
-            tryStartAnimOut();
-            mCirculatingDy = 0; // reset
-        } else if (mCirculatingDy < ANIM_IN_THRESHOLD) {
-            tryStartAnimIn();
-            mCirculatingDy = 0;
-        }
     }
 
-    private void tryStartAnimIn() {
-        // avoid repeat
-        if (mLastScrollDirectionUp) return;
-        mLastScrollDirectionUp = true;
+    @Override
+    public void startAnimUp() {
         if (mHeaderViewAnimator != null) {
             mHeaderViewAnimator.cancel();
             mHeaderViewAnimator.setIntValues(mNewTop, 0);
@@ -77,10 +34,8 @@ public class ScrollingHeaderBehavior extends CoordinatorLayout.Behavior {
         }
     }
 
-    private void tryStartAnimOut() {
-        // avoid repeat
-        if (!mLastScrollDirectionUp) return;
-        mLastScrollDirectionUp = false;
+    @Override
+    public void startAnimDown() {
         if (mHeaderViewAnimator != null) {
             mHeaderViewAnimator.cancel();
             mHeaderViewAnimator.setIntValues(mNewTop, -mHeaderViewHeight);
@@ -88,10 +43,9 @@ public class ScrollingHeaderBehavior extends CoordinatorLayout.Behavior {
         }
     }
 
-    private void setupAnimator() {
+    @Override
+    void setupAnimator() {
         mNewTop = 0;
-        // the first anim must be out, so last must be in
-        mLastScrollDirectionUp = true;
         mHeaderViewAnimator = ValueAnimator.ofInt(mNewTop, -mHeaderViewHeight);
         mHeaderViewAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
